0eb805e40731627d461a11795164faed74d7320b
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-09-05 13:20:24 +0430

    Add file structure starter, fix #3

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      Hi this is a starter project
+      Hi this is a starter project1
     </div>
   )
 }
